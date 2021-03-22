278d689402118e254d2a0fa4fe38b9c1712e2ee7
Sabiq Khan <SabiqKhan@Sabiqs-MacBook-Pro.local>
2021-03-22 12:46:12 +0000

    Updates

---
+++ precache-manifest.39f29c79564cbb4054aba691255205ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c873d188ac798ce51c5c024de03284",
+    "url": "/Sabflix/index.html"
+  },
+  {
+    "revision": "76a5d32636f325c64eb7",
+    "url": "/Sabflix/static/css/main.2d5ea042.chunk.css"
+  },
+  {
+    "revision": "f2d09eb8d34a95f695b1",
+    "url": "/Sabflix/static/js/2.e4fcfd99.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Sabflix/static/js/2.e4fcfd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a5d32636f325c64eb7",
+    "url": "/Sabflix/static/js/main.2b8a044a.chunk.js"
+  },
+  {
+    "revision": "d39f49062eb1ee104b6e",
+    "url": "/Sabflix/static/js/runtime-main.ba279f11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sabflix/static/media/logo.5d5d9eef.svg"
+  }
+]);
